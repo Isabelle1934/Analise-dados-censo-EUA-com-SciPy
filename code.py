07de56f19ad91cabe473924d525ae1a3d078df70
Isabelle Silva <bellinha292@gmail.com>
2026-04-20 22:13:34 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+
+c = ("/content/moved_same_state.csv")
+v = ("/content/moved_between_states.csv")
+control = pd.read_csv(c)
+variant = pd.read_csv(v)
+
+# control.head()
+# variant.head()
+
+county = pd.DataFrame()
+state = pd.DataFrame()
+division = pd.DataFrame()
+region = pd.DataFrame()
